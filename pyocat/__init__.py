1204a2d985f51ba1a1103df0a750b54f9269ee5d
Noah.Abfalterer <Noah.Abfalterer@watercryst.com>
2026-07-28 10:08:36 +0200

    fix: Added alias to exceptions in __init__.py to satisfy ruff checks.

--- pyocat/__init__.py
+++ pyocat/__init__.py
@@ -4,5 +4,6 @@ from .api_client import ApiClient as ApiClient
 from .async_auth import AsyncAuth as AsyncAuth
 from .async_api_client import AsyncApiClient as AsyncApiClient
 
-from .exceptions import WTCApiUnauthorizedError, WTCApiDisabledError, \
-    WTCApiTemporaryError
+from .exceptions import WTCApiUnauthorizedError as WTCApiUnauthorizedError
+from .exceptions import WTCApiDisabledError as WTCApiDisabledError
+from .exceptions import WTCApiTemporaryError as WTCApiTemporaryError
